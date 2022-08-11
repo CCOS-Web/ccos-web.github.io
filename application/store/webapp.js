425685f5e96d11a0ae4707c13b7947d53d562b5f
CCOS-Web <102447804+CCOS-Web@users.noreply.github.com>
2022-08-11 12:00:10 +0800

    Add files via upload

--- application/store/webapp.js
+++ application/store/webapp.js
@@ -1,8 +1,8 @@
 var webapp=[
-	["name", "url", "icon", "web url"],
-	["Chat", "application/etc/code/chat.html", "../etc/icon/chat.png"],
-	["CucumberCan", "application/etc/code/ccan.html", "", "https://cucumbercan.github.io/index.html"],
+ // ["name", "url", "icon", "web url"],
+	["Chat", "application/etc/code/chat.html", "../etc/icon/chat.png", "https://static-7f9768c6-c545-4f94-bd4a-cd62cb0086e3.bspapp.com/application/etc/code/chat.html"],
 	["Paint", "https://jspaint.app", "https://jspaint.app/favicon.ico"],
+	["WereWolf Kill", "application/etc/code/wwk.html", "../../img/wwk.png"]
 ]
 
 webapp.sort(function(l1,l2){

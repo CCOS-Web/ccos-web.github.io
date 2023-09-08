7e6bab7cee3e0bef2f4c203aef3c62c5c246092f
berniehuang2008 <berniehuang2008@163.com>
2023-09-08 16:44:43 +0800

    fsd

--- 3.0/application/store/webapp.js
+++ 3.0/application/store/webapp.js
@@ -1,11 +1,12 @@
-var webapp=[
- // ["name", "url", "icon", "web url"],
+var webapp = [
+	// ["name", "url (relative path in desktop.html)", "icon (relative path in store.html)", "web url", "paras"],
 	["Chat", "application/etc/code/chat.html", "../etc/icon/chat.png"],
 	["Paint", "https://jspaint.app", "https://jspaint.app/favicon.ico"],
 	["WereWolf", "application/etc/code/wwk.html", "../../img/wwk.png"],
 	["Power Sheet", "https://hgaka.github.io/sheet", "https://static-801c4008-56be-4eb8-8499-377f12d55817.bspapp.com/PowerSTD/PowerSheet/favicon.png"],
+	["G-Chat", "application/gchat/ext/red_packet/RPCover.html", "../../icon/gchat.png", null, "nobg"],
 ]
 
-webapp.sort(function(l1,l2){
-	return l1[0]<l2[0]?-1:1;
+webapp.sort(function (l1, l2) {
+	return l1[0] < l2[0] ? -1 : 1;
 });
